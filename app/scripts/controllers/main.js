6aa92517b9b8f440a466a9e46e66522f9574c40e
Jack Ketcham <kcajketcham@gmail.com>
2014-01-27 14:44:35 -0700

    Initial commit, bro

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('bronameApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
